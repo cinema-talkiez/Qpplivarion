6fa7e82fc582e704bf4670ddcc9ea49493d363c6
cinema-talkiez <cinema.talkiez.io@gmail.com>
2025-07-16 14:09:21 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone', // ✅ This is required for Railway custom server deploy
 };
 
 export default nextConfig;
